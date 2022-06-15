5f8b66774b6f49df41f229bf7d06f72589399ee0
Jadson Sanches <jadson.sanches@7comm.com.br>
2022-06-15 14:05:46 -0300

    Simple example about serverless scheduled task

---
+++ handler.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports.run = async (event, context) => {
+  const time = new Date();
+  console.log(`Your cron function "${context.functionName}" ran at ${time}`);
+  console.log("================================================");
+  console.log("");
+};
